ec52b638e80072fde9feab5b81f4526c4b13464e
itsbibek1122 <dhunganabibek1@gmail.com>
2023-08-15 21:31:25 +0545

    error on yesterday work corrected

--- grihasti/lib/screens/add_property/model/submit.dart
+++ grihasti/lib/screens/add_property/model/submit.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -10,13 +9,13 @@ import 'package:provider/provider.dart';
 class FirebaseService {
   static Future<void> savePropertyData(PropertyData propertyData) async {
     try {
-      // Get the Firestore instance
       FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-      // Generate a unique ID for the property
       String propertyId = firestore.collection('properties').doc().id;
 
-      // Store the property data under the 'properties' collection with the generated ID
+      // Upload the images and get the image URLs
+      List<String> imageUrls =
+          await uploadPropertyImages(propertyId, propertyData.images);
+
       await firestore.collection('properties').doc(propertyId).set({
         'userId': propertyData.userId,
         'ownerName': propertyData.ownerName,
@@ -28,64 +27,41 @@ class FirebaseService {
         'location': propertyData.location,
         'detailedLocation': propertyData.detailedLocation,
         'propertyDescription': propertyData.propertyDescription,
-        'images': [], // This will be updated later
-
-        // Add other fields as needed
+        'images': imageUrls, // Store the image URLs in the Firestore document
       });
-
-      // Handle image upload separately, assuming you have a list of image URLs in propertyData
-      await uploadPropertyImagesAsLinks(propertyId, propertyData.images);
     } catch (e) {
-      // Handle errors appropriately
       print("Error saving property data: $e");
     }
   }
 
-  static Future<void> uploadPropertyImagesAsLinks(
+  static Future<List<String>> uploadPropertyImages(
       String propertyId, List<String> imageFilePaths) async {
-    try {
-      // Get the Firebase Storage instance
-      FirebaseStorage storage = FirebaseStorage.instance;
+    List<String> imageUrls = [];
 
-      // Create a reference to the property's image folder in Firebase Storage
+    try {
+      FirebaseStorage storage = FirebaseStorage.instance;
       Reference propertyImageRef =
           storage.ref().child('properties/$propertyId');
 
-      // Upload each image to Firebase Storage and get the download URL
       for (int i = 0; i < imageFilePaths.length; i++) {
-        String imageName =
-            'image_$i.jpg'; // You can customize the image name if needed
+        String imageName = 'image_$i.jpg';
         String imageFilePath = imageFilePaths[i];
-
         File imageFile = File(imageFilePath);
         if (imageFile.existsSync()) {
-          // Upload the file to Firebase Storage.
-          UploadTask uploadTask =
-              propertyImageRef.child(imageName).putFile(imageFile);
-          await uploadTask
-              .whenComplete(() => print('Image $imageName uploaded'));
+          TaskSnapshot uploadTask =
+              await propertyImageRef.child(imageName).putFile(imageFile);
 
-          // Get the download URL for the file.
-          String downloadUrl = await uploadTask.snapshot.ref.getDownloadURL();
-
-          // Store the download URL in Firestore.
-          await FirebaseFirestore.instance
-              .collection('properties')
-              .doc(propertyId)
-              .update({
-            'images': [downloadUrl]
-          });
+          // Get the download URL for the uploaded image
+          String imageUrl = await uploadTask.ref.getDownloadURL();
+          imageUrls.add(imageUrl);
         } else {
-          // Create the file.
-          // imageFile.createSync();
-
           print('Error: File not found at path $imageFilePath');
-          // Upload the file to Firebase Storage.
         }
       }
     } catch (e) {
-      // Handle errors appropriately
       print("Error uploading property images: $e");
     }
+
+    return imageUrls; // Return the list of image URLs
   }
 }
