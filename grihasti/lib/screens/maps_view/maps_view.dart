acbb123be38e5f7a1c431740c7944ca54f926826
itsbibek1122 <dhunganabibek1@gmail.com>
2023-08-19 10:11:24 +0545

    Property map page complete with algorithm

--- grihasti/lib/screens/maps_view/maps_view.dart
+++ grihasti/lib/screens/maps_view/maps_view.dart
@@ -1,22 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:grihasti/provider/current_location_provider.dart';
-import 'package:grihasti/provider/location_provider.dart';
 import 'package:grihasti/screens/homescreen/components/custom_appbar.dart';
 import 'package:grihasti/screens/homescreen/components/other_products.dart';
-import 'package:grihasti/screens/maps_view/location_permission.dart';
-import 'package:grihasti/screens/maps_view/map_algo.dart';
-import 'package:grihasti/utils/showSnackBar.dart';
 import 'package:grihasti/utils/style/colors.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
 
 class MapsView extends StatefulWidget {
-  const MapsView({super.key});
-
   @override
   State<MapsView> createState() => _MapsViewState();
 }
@@ -25,37 +18,60 @@ class _MapsViewState extends State<MapsView> {
   final CollectionReference propertiesCollection =
       FirebaseFirestore.instance.collection('properties');
 
+  late LatLng userLocation = LatLng(27.700769, 85.300140); // Default location
+
+  @override
+  void initState() {
+    super.initState();
+    _updateUserLocation();
+  }
+
+  Future<void> _updateUserLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        userLocation = LatLng(position.latitude, position.longitude);
+      });
+    } catch (e) {
+      print('Unable to access location: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: 'Property Map'),
+      appBar: MyAppBar(title: 'Property Maps'),
       body: StreamBuilder<QuerySnapshot>(
         stream: propertiesCollection.snapshots(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
-          final markers = <Marker>[];
-
-          if (snapshot.hasData) {
-            for (final doc in snapshot.data!.docs) {
-              final geoPoint = doc['location'] as GeoPoint;
-              final latLng = LatLng(geoPoint.latitude, geoPoint.longitude);
-
-              markers.add(
-                Marker(
-                  width: 30.0,
-                  height: 30.0,
-                  point: latLng,
-                  builder: (ctx) => const Icon(
-                    Icons.home,
-                    color: AppColors.blackColor,
-                    size: 42,
-                  ),
-                ),
-              );
-            }
+          if (!snapshot.hasData || snapshot.data == null) {
+            return Center(child: Text('No data available.'));
           }
+          final markers = snapshot.data!.docs.map((doc) {
+            final geoPoint = doc['location'] as GeoPoint;
+            final latLng = LatLng(geoPoint.latitude, geoPoint.longitude);
+
+            return Marker(
+              width: 30.0,
+              height: 30.0,
+              point: latLng,
+              builder: (ctx) => GestureDetector(
+                onTap: () {
+                  _showPropertyDetailsDialog(context, doc);
+                },
+                child: const Icon(
+                  Icons.home,
+                  color: Colors.black,
+                  size: 42,
+                ),
+              ),
+              anchorPos: AnchorPos.align(AnchorAlign.top),
+            );
+          }).toList();
 
           return FlutterMap(
             options: MapOptions(
@@ -74,11 +90,63 @@ class _MapsViewState extends State<MapsView> {
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: AppColors.blackColor,
         onPressed: () => _showNearestPropertyDialog(),
-        child: Icon(Icons.location_searching),
+        icon: Icon(Icons.near_me_outlined),
+        label: Text('Nearest Property'),
       ),
     );
+  }
+
+  void _showPropertyDetailsDialog(BuildContext context, DocumentSnapshot doc) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        final geoPoint = doc['location'] as GeoPoint;
+        final documentId = doc.id;
+
+        final propertyLatLng = LatLng(geoPoint.latitude, geoPoint.longitude);
+
+        final distance = calculateDistance(userLocation, propertyLatLng);
+
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Property Details',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+              SizedBox(height: 8),
+
+              Text('Distance: ${distance.toStringAsFixed(2)} meters'),
+              Text('Title: ${doc['propertyTitle']}'),
+              Text('Location: ${doc['detailedLocation']}'),
+              Text('Price: ${doc['price']}'),
+              Text('Purpose: ${doc['purpose']}'),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/details',
+                        arguments: documentId);
+                  },
+                  child: Text('Show details')),
+
+              // Add more details as needed
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  double calculateDistance(LatLng from, LatLng to) {
+    final double distance = Distance().as(
+      LengthUnit.Meter,
+      LatLng(from.latitude, from.longitude),
+      LatLng(to.latitude, to.longitude),
+    );
+    return distance;
   }
 
   void _showNearestPropertyDialog() async {
